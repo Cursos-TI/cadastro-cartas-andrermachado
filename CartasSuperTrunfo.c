0860e4351c40e7aa92f6003db738e11892991594
André Renato Machado <147953691+andrermachado@users.noreply.github.com>
2026-06-02 00:28:13 +0000

    inclusao cidade 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,13 +6,13 @@
 
 int main() {
 
-  //Definição das variáveis
-  int carta, codigo_carta, populacao, area, PIB, pontos;
-    char cidade[20], estado[2];
+  // Definição das variáveis
+  int codigo_carta, codigo_carta1, populacao, populacao1, area, area1, PIB, PIB1;
+  char cidade[20], cidade1[20], estado[3], estado1[3];
 
   // Entrada de dados
-    printf("Digite o codigo da carta: \n");
-    scanf("%d", &codigo_carta);
+  printf("Digite o codigo da carta: \n");
+  scanf("%d", &codigo_carta);
 
     printf("Digite a cidade: \n");
     scanf("%s", cidade);
@@ -29,6 +29,25 @@ int main() {
     printf("Digite o estado: \n");
     scanf("%s", estado);
 
+    printf("Digite o codigo da carta:\n");
+    scanf("%d", &codigo_carta1);
+
+    printf("Digite a cidade: \n");
+    scanf("%s", cidade1);
+
+    printf("Digite a populacao: \n");
+    scanf("%d", &populacao1);
+
+    printf("Digite a area: \n");
+    scanf("%d", &area1);
+
+    printf("Digite o PIB1: \n");
+    scanf("%d", &PIB1);
+    
+    printf("Digite o estado1: \n");
+    scanf("%s", estado1);
+
+    
     // exibição de dados
 
     printf("Cidade: %s\n", cidade);
@@ -36,6 +55,13 @@ int main() {
     printf("Area: %d\n", area);
     printf("PIB: %d\n", PIB);
     printf("Estado: %s\n", estado);
+    printf("Cidade: %s\n", cidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %d\n", area1);
+    printf("PIB: %d\n", PIB1);
+    printf("Estado: %s\n", estado1);
+
+
  
 
     // Área para definição das variáveis para armazenar as propriedades das cidades
